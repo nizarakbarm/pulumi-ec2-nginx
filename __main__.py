aed914490e56aff15077c141c5878b6dd61acb99
nizarakbarm <nizar@domainesia.com>
2023-11-08 18:24:14 +0700

    fix: Fix missing ingress rule at Security Group

--- __main__.py
+++ __main__.py
@@ -16,10 +16,13 @@ print("Create security group...")
 # Create Security Group
 security_group = aws.ec2.SecurityGroup("http-port",
         description="Allow HTTP traffic",
-        from_port=80,
-        to_port=80,
-        protocol="tcp",
-        cidr_blocks=['0.0.0.0/0']
+        ingress=[aws.ec2.SecurityGroupIngressArgs(
+            description="Allow port 80",
+            from_port=80,
+            to_port=80,
+            protocol="tcp",
+            cidr_blocks=['0.0.0.0/0'],
+        )]
 )
 print("Security group creation complete...")
 
@@ -29,7 +32,7 @@ server = aws.ec2.Instance("nginx",
         ami="ami-0ebcd68de1afe59cd",
         instance_type="t2.micro",
         user_data=user_data,
-        vpc_security_group_ids=[security_group.id]                         
+        vpc_security_group_ids=[security_group.id],                      
 )
 print("EC2 Instance Creation Complete...")
 
