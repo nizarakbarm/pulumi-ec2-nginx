260db42fe7cf3ab3f789967e44567c51a88f26eb
nizarakbarm <nizar@domainesia.com>
2023-11-08 22:36:18 +0700

    feat: Print http_ec2_ip

--- __main__.py
+++ __main__.py
@@ -64,6 +64,7 @@ server = aws.ec2.Instance("nginx",
 print("EC2 Instance Creation Complete...")
 
 http_ec2_ip='http://{ip}'.format(ip=str(server.public_ip))
+print(http_ec2_ip)
 r = requests.get(str(http_ec2_ip))
 
 pulumi.export('public_ip', server.public_ip)
