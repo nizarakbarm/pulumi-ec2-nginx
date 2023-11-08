be632771f23ec901053d2e1ece42126020295d00
nizarakbarm <nizar@domainesia.com>
2023-11-08 15:11:05 +0700

    feat: Add __main__.py

---
+++ __main__.py
@@ -0,0 +1,32 @@
+import pulumi
+import pulumi_aws as aws
+
+
+# user data for install dependency
+user_data = """
+#!/bin/bash
+sudo yum update -y
+sudo yum upgrade -y
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+"""
+
+# Create Security Group
+security_group = aws.ec2.SecurityGroup("http-port",
+        description="Allow HTTP traffic",
+        from_port=80,
+        to_port=80,
+        protocol="tcp",
+        cidr_blocks=['0.0.0.0/0']
+)
+
+# Create EC2 Instance
+server = aws.ec2.Instance("nginx",
+        ami="ami-0ebcd68de1afe59cd",
+        instance_type="t2.micro",
+        user_data=user_data,
+        vpc_security_group_ids=[security_group.id]                         
+)
+
+pulumi.export('public_ip', server.public_ip)
