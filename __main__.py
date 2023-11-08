56ed5e07d983817019d3efb0d71b6d4cd380810b
nizarakbarm <nizar@domainesia.com>
2023-11-08 19:31:39 +0700

    feat: Add security group ingress port 22 and add key pair to ec2

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import os
 import pulumi
 import pulumi_aws as aws
 
@@ -14,17 +15,28 @@ sudo systemctl start nginx
 
 print("Create security group...")
 # Create Security Group
-security_group = aws.ec2.SecurityGroup("http-port",
-        description="Allow HTTP traffic",
-        ingress=[aws.ec2.SecurityGroupIngressArgs(
+security_group = aws.ec2.SecurityGroup("Security Group EC2",
+        description="Security Group EC2",
+        ingress=[
+            aws.ec2.SecurityGroupIngressArgs(
             description="Allow port 80",
             from_port=80,
             to_port=80,
             protocol="tcp",
-            cidr_blocks=['0.0.0.0/0'],
-        )]
+            cidr_blocks=['0.0.0.0/0']),
+            aws.ec2.SecurityGroupIngressArgs(
+            description="Allow port 22",
+            from_port=22,
+            to_port=22,
+            protocol="tcp",
+            cidr_blocks=['0.0.0.0/0']),
+            )
+            ]
 )
 print("Security group creation complete...")
+
+print("Create AWS EC2 KeyPair...")
+key_pair = aws.ec2.KeyPair("key-pair",public_key=os.getenv('PUBLIC_KEY'))
 
 print("Create EC2 Instance...")
 # Create EC2 Instance
